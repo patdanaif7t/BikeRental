a5acfd43df7ed79eaccfe4841d93a18098c52fe6
patdanaif7t <spermtv.ch@gmail.com>
2018-11-02 13:23:59 +0700

    put Databese

---
+++ src/bikerental/Database.java
@@ -0,0 +1,62 @@
+
+package bikerental;
+
+
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+
+
+public class Database {
+    private static MongoClientURI uri;
+    private static MongoClient client;
+    private static MongoDatabase db;
+    private static MongoCollection<Document> collection;
+
+    public Database(){
+        uri  = new MongoClientURI("mongodb://admin:12345678za@ds249583.mlab.com:49583/bikerental");
+        client = new MongoClient(uri);
+        db = client.getDatabase(uri.getDatabase());
+    }
+    
+    public static void setCollection(String col) {
+        collection = db.getCollection(col);
+    }
+    
+    public static MongoCollection<Document> getCollection() {
+        return collection;
+    }
+    
+    public static boolean insert(Document doc) {
+        try {
+            collection.insertOne(doc);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+   
+    public static Document find(String key,
+            String value) {
+        return collection.find(eq(key, value)).first();
+    }
+    
+    public static Document find(String key,
+            int value) {
+        return collection.find(eq(key, value)).first();
+    }
+     public static FindIterable<Document> findAll(MongoCollection<Document> col) {
+        return collection.find();
+    }
+
+//    Document find(String id, int id0) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    
+
+     
+}
